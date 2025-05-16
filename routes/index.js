c2790ed0f7c93501d69e9a670cce7092e3fde2d1
Elhama Tokhi <tokhielaham30@gmail.com>
2025-05-16 23:13:45 +0200

    Add add.js page

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,15 @@
 import { Router } from 'express'
-
+import { readFileSync } from 'fs'
+import { fileURLToPath } from 'url'
+import path from 'path'
+import fs from 'fs'
 const router = Router()
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+// To store the proverbs
+let data
 
 router.get('/', (req, res) => {
   res.render('index')
@@ -8,15 +17,45 @@ router.get('/', (req, res) => {
 
 // Get all proverbs
 router.get('/proverbs', (req, res) => {
-  res.render('proverbs')
+  const proverbsJSON = readFileSync(
+    path.join(__dirname, '../proverbs.json'),
+    'utf-8'
+  )
+  let proverbs = JSON.parse(proverbsJSON)
+
+  // Bonus - list proverbs by Categories
+  const selectedCategory = req.query.category || ''
+
+  if (selectedCategory) {
+    proverbs = proverbs.filter(p => p.category === selectedCategory)
+  }
+
+  res.render('proverbs', { proverbs, selectedCategory })
 })
 
 // Get a single proverb by ID
-router.get('/proverbs/:id', (req, res) => {})
+router.get('/proverbs/:id', (req, res) => {
+
+})
 
 // Add a new proverb
-router.post('/proverbs', (req, res) => {
-  console.log('A new proverb added!')
+
+router.get('/add', (req, res) => {
+  res.render('add')
+})
+
+router.post('/add', (req, res) => {
+  const newProverb = {
+    textDari : req.body.textDari,
+    textPashto : req.body.textPashto,
+    translation: req.body.translationEn,
+    meaning: req.body.meaning,
+    category: req.body.category
+
+  }
+  console.log(newProverb)
+
+  res.redirect('/',{proberb})
 })
 
 // Update an existing proverb
@@ -27,11 +66,6 @@ router.put('/proverbs/:id', (req, res) => {
 //Delete a proverb
 router.delete('/proverbs/:id', (req, res) => {
   console.log(`A proverb was just deleted. 😒`)
-})
-
-// Bonus Features
-router.get('/proverbs/:categori', (req, res) => {
-  console.log(`Filter proverbs by categori using query parameters`)
 })
 
 // Endpoint Random Proverb
